6b169fffa205e1548be22828b722978dd087d7c1
Astroth1984 <elmounir.othmane666@gmail.com>
2023-01-11 22:08:43 +0100

    Request Mapping for rating data controller

--- rating-data-service/src/main/java/com/codxdoc/ratingdataservice/resources/RatingResourceController.java
+++ rating-data-service/src/main/java/com/codxdoc/ratingdataservice/resources/RatingResourceController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codxdoc.ratingdataservice.models.Rating;
+
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingResourceController {
