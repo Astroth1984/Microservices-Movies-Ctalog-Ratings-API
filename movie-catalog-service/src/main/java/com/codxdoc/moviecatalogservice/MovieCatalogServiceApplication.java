1757254acd6a3c80c7d62b5ec44495c91aa24121
Astroth1984 <elmounir.othmane666@gmail.com>
2023-01-12 00:19:51 +0100

    Add Bean provider for RestTemplate instance

--- movie-catalog-service/src/main/java/com/codxdoc/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/codxdoc/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,9 +2,16 @@ package com.codxdoc.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		System.getProperties().put( "server.port", 8182 );
