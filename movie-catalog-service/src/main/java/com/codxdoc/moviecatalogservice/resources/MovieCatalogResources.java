10c11446c962160ba5614d8bb50403dc67314323
Astroth1984 <elmounir.othmane666@gmail.com>
2023-01-12 02:17:29 +0100

    WebClient Reactive Spring boot Asynchronous call

--- movie-catalog-service/src/main/java/com/codxdoc/moviecatalogservice/resources/MovieCatalogResources.java
+++ movie-catalog-service/src/main/java/com/codxdoc/moviecatalogservice/resources/MovieCatalogResources.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.codxdoc.moviecatalogservice.models.CatalogItem;
 import com.codxdoc.moviecatalogservice.models.Movie;
@@ -22,8 +23,14 @@ public class MovieCatalogResources {
 	@Autowired
 	private RestTemplate restTemplate; 
 	
+	@Autowired
+	private 
+	WebClient.Builder webClientBuiler;
+	
+	
 	@GetMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+		
 		// get all rated movies IDs - hard coded Ratings (for now)
 		// Assuming this is th response from Rating-Data-Api
 		List<Rating> ratings = Arrays.asList(
@@ -33,7 +40,15 @@ public class MovieCatalogResources {
 				);
 		
 		return ratings.stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:8181/movies/" + rating.getMovieId(), Movie.class);
+			//Movie movie = restTemplate.getForObject("http://localhost:8181/movies/" + rating.getMovieId(), Movie.class);
+			
+			Movie movie = webClientBuiler.build()
+				.get()
+				.uri("http://localhost:8181/movies/"+ rating.getMovieId())
+				.retrieve()
+				.bodyToMono(Movie.class)
+				.block();
+			
 			return new CatalogItem(movie.getName(), "some Desc", rating.getRating());
 		})
 		.collect(Collectors.toList());
